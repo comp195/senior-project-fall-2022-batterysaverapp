25d6650b168967d6a566a54c6182624295132fe5
ambamore2000 <a_kim86@u.pacific.edu>
2022-10-10 20:18:52 -0700

    init and complete getBatteryPercentage() function

--- app/src/main/java/com/example/ecobattery/library/BatteryInformation.java
+++ app/src/main/java/com/example/ecobattery/library/BatteryInformation.java
@@ -1,5 +1,11 @@
 package com.example.ecobattery.library;
 
+import android.os.BatteryManager;
+
 public class BatteryInformation {
+
+    public int getBatteryPercentage() {
+        return BatteryManager.BATTERY_PROPERTY_CAPACITY;
+    }
 
 }
