36b39b04b2c5b1d2200a80c204e264059aebd70b
ambamore2000 <a_kim86@u.pacific.edu>
2022-10-10 20:36:57 -0700

    make getBatteryPercentage() and getEstimatedTime() functions static and fix getBatteryPercentage() function

--- app/src/main/java/com/example/ecobattery/library/BatteryInformation.java
+++ app/src/main/java/com/example/ecobattery/library/BatteryInformation.java
@@ -1,11 +1,23 @@
 package com.example.ecobattery.library;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.BatteryManager;
 
 public class BatteryInformation {
 
-    public int getBatteryPercentage() {
-        return BatteryManager.BATTERY_PROPERTY_CAPACITY;
+    /*
+    TODO Test with drained battery
+     */
+    public static float getBatteryPercentage(Context context) {
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, intentFilter);
+
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        return (level * 100) / (float)scale;
     }
 
     /*
@@ -13,9 +25,8 @@ public class BatteryInformation {
     Research suggests we should gather data to analyze.
     https://stackoverflow.com/questions/30208271/calculating-remaining-time-of-battery-live
      */
-    public int getEstimatedTime() {
+    public static int getEstimatedTime() {
         return -1;
     }
-
 
 }
