8aa45860c3b3999cbbd0cc8d31914b16848d3e9b
ambamore2000 <a_kim86@u.pacific.edu>
2022-10-10 20:04:07 -0700

    init dataactivity and saveractivity

---
+++ app/src/main/java/com/example/ecobattery/SaverActivity.java
@@ -0,0 +1,14 @@
+package com.example.ecobattery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saver);
+    }
+}
