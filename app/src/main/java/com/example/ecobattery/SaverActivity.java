068d66195b2d940d65be047a5048e55ecd9a61b8
ambamore2000 <a_kim86@u.pacific.edu>
2022-10-24 22:09:28 -0700

    create saver activity

---
+++ app/src/main/java/com/example/ecobattery/SaverActivity.java
@@ -0,0 +1,19 @@
+package com.example.ecobattery;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SaverActivity extends AppCompatActivity {
+
+    /*
+    TODO: Consider live update on information
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.saver_activity);
+
+    }
+}
