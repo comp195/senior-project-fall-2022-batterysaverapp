532100da4be3f3973b083ce107c0ebfd009be924
ambamore2000 <a_kim86@u.pacific.edu>
2022-10-24 22:01:37 -0700

    add getApplicationsList() function and add test code for terminateApplications() function

--- app/src/main/java/com/example/ecobattery/library/Optimization.java
+++ app/src/main/java/com/example/ecobattery/library/Optimization.java
@@ -1,8 +1,12 @@
 package com.example.ecobattery.library;
 
-import android.app.ActivityManager;
 import android.content.Context;
-import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class Optimization {
 
@@ -14,9 +18,55 @@ public class Optimization {
         appContext = newContext;
     }
 
+    public List<ApplicationInfo> getListOfApplications() {
+        /*
+        try
+        {
+            System.out.println("INSTALLED");
+            List<ApplicationInfo> xyz = getApplicationContext().getPackageManager().getInstalledApplications(0);
+            for (ApplicationInfo a : xyz) {
+                // If app info is non-system
+                if ((a.flags & ApplicationInfo.FLAG_SYSTEM) == 0)
+                    System.out.println(a.packageName);
+            }
+            Drawable icon = getApplicationContext().getPackageManager().getApplicationIcon("com.google.android.networkstack.tethering");
+            ImageView imageView = (ImageView)findViewById(R.id.imageView2);
+            imageView.setImageDrawable(icon);
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+         */
+        return null;
+    }
+
     //TODO Root android device and save battery!
     public void terminateApplications() {
         // TODO
+        Process process = null;
+        try {
+            //process = Runtime.getRuntime().exec("ps -A");
+            process = Runtime.getRuntime().exec("pm list packages -3");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert process != null;
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()));
+        String s = null;
+
+        while (true)
+        {
+            try {
+                if ((s = bufferedReader.readLine()) == null) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(s);
+        }
+        System.out.println("Made it");
     }
 
 }
