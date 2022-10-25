af141bd57ba5f9cc690fdf21a9402d588eed86fa
ambamore2000 <a_kim86@u.pacific.edu>
2022-10-24 22:56:51 -0700

    add horizontal swipe code for saver activity

--- app/src/main/java/com/example/ecobattery/SaverActivity.java
+++ app/src/main/java/com/example/ecobattery/SaverActivity.java
@@ -3,17 +3,18 @@ package com.example.ecobattery;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.ecobattery.library.SaverPagerAdapter;
 
 public class SaverActivity extends AppCompatActivity {
-
-    /*
-    TODO: Consider live update on information
-     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.saver_activity);
 
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new SaverPagerAdapter(this));
     }
 }
