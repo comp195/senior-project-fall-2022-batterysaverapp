4cd31a02813b6ae2f70b81445695afbf0935fcbb
AlihaNaeem <s_naeem@u.pacific.edu>
2022-11-25 17:06:53 -0800

    battery information test added

---
+++ app/src/test/java/com/example/ecobattery/library/BatteryInformationTest.java
@@ -0,0 +1,36 @@
+package com.example.ecobattery.library;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BatteryInformationTest {
+
+    @Test
+    void setAppContext() {
+    }
+
+    @Test
+    void getAppContext() {
+    }
+
+    @Test
+    void getBatteryPercentage() {
+    }
+
+    @Test
+    void getEstimatedTime() {
+    }
+
+    @Test
+    void isCharging() {
+    }
+
+    @Test
+    void isLowBattery() {
+    }
+
+    @Test
+    void getTimeToCharge() {
+    }
+}
