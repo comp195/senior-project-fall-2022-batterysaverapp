c789fec73f8a46e9fa41a4c8cc6b0930175244f2
AlihaNaeem <s_naeem@u.pacific.edu>
2022-11-25 16:55:48 -0800

    Information activity test added

---
+++ app/src/test/java/com/example/ecobattery/library/InformationActivityTest.java
@@ -0,0 +1,22 @@
+package com.example.ecobattery.library;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InformationActivityTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void attachBaseContext() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onCreate() {
+    }
+}
