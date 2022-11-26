ae6b0554209a00b0d5d1bbb131a035260ea7ce8f
AlihaNaeem <s_naeem@u.pacific.edu>
2022-11-25 17:24:51 -0800

    display testing

---
+++ app/src/test/java/com/example/ecobattery/library/DisplayAppTest.java
@@ -0,0 +1,24 @@
+package com.example.ecobattery.library;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DisplayAppTest {
+
+    @Test
+    void getPackageName() {
+    }
+
+    @Test
+    void isSelected() {
+    }
+
+    @Test
+    void getIcon() {
+    }
+
+    @Test
+    void getName() {
+    }
+}
