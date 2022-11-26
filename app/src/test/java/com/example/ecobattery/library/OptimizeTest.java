24bd6b224472f6f90ab8b8bbda43014ed8072bc6
AlihaNaeem <s_naeem@u.pacific.edu>
2022-11-25 17:23:50 -0800

    optimization testing

---
+++ app/src/test/java/com/example/ecobattery/library/OptimizeTest.java
@@ -0,0 +1,16 @@
+package com.example.ecobattery.library;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OptimizeTest {
+
+    @Test
+    void runOptimization() {
+    }
+
+    @Test
+    void forceStopApplications() {
+    }
+}
